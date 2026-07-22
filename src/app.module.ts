13bd81063fa3750524d4f0bf23e6aeb69f2ddc62
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2026-07-22 14:28:02 -0700

    create alias imports config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { LoggingModule } from './common/logging/logging.module';
-import { SecurityModule } from './common/security/security.module';
-import { TenantModule } from './common/tenant/tenant.module';
-import { AppConfigModule } from './config/app-config.module';
-import { DatabaseModule } from './database/database.module';
-import { HealthModule } from './modules/health/health.module';
-import { AuthenticationModule } from './authentication/authentication.module';
+import { AuthenticationModule } from '@/authentication/authentication.module';
+import { LoggingModule } from '@/common/logging/logging.module';
+import { SecurityModule } from '@/common/security/security.module';
+import { TenantModule } from '@/common/tenant/tenant.module';
+import { AppConfigModule } from '@/config/app-config.module';
+import { DatabaseModule } from '@/database/database.module';
+import { HealthModule } from '@/modules/health/health.module';
 
 @Module({
   imports: [
--- src/env.loader.ts
+++ src/env.loader.ts
@@ -1,4 +1,10 @@
+import { register } from 'tsconfig-paths';
 import { config } from 'dotenv';
 import { join } from 'path';
+
+register({
+  baseUrl: join(__dirname),
+  paths: { '@/*': ['*'] },
+});
 
 config({ path: join(process.cwd(), '.env') });
